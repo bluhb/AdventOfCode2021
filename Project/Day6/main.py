91dda07f7b8c7e8ef26d183eba0e00cb9001db78
Bluhb_ <rogiervtol@gmail.com>
2021-12-06 22:55:54 +0100

    optimized solutions

--- Project/Day6/main.py
+++ Project/Day6/main.py
@@ -27,6 +27,16 @@ def runSim(days, fish):
         fish.list[8] += reproducing # add fish with timer 8
     print(f"Solution after {days}: {sum(fish.list)}")
 
+def runSimV2(days, inputFile):
+    l = [0]*9
+    a = readInput(inputFile)
+    for i in set(a): l[i] += a.count(i)
+    for i in range(0,days):
+        reproducing = l.pop(0)
+        l[6] += reproducing # reset timer to 6
+        l.append(reproducing) # add fish with timer 8
+    print(f"Solution after {days}: {sum(l)}")
+
 @time
 def solution1():
     l = readInput("input.txt")
@@ -39,5 +49,24 @@ def solution2():
     fish = FishList(l)
     runSim(256, fish)
 
+@time
+def solution1v2():
+    runSimV2(80, "input.txt")
+
+@time
+def solution2v2():
+    runSimV2(256, "input.txt")
+
 solution1()
 solution2()
+solution1v2()
+solution2v2()
+
+# Solution after 80: 373378
+# Function solution1(): time elapsed: 1.050 [ms]
+# Solution after 256: 1682576647495
+# Function solution2(): time elapsed: 0.646 [ms]
+# Solution after 80: 373378
+# Function solution1v2(): time elapsed: 0.235 [ms]
+# Solution after 256: 1682576647495
+# Function solution2v2(): time elapsed: 0.312 [ms]
