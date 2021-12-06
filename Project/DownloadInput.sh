c1a2b43e38ddef9d531f377492ce5acae20b9e0b
Bluhb_ <rogiervtol@gmail.com>
2021-12-06 22:32:25 +0100

    made day 6 a little bit faster

--- Project/DownloadInput.sh
+++ Project/DownloadInput.sh
@@ -2,8 +2,9 @@ echo "Which day is it?: "
 read Day
 mkdir Day$Day
 touch Day$Day/main.py Day$Day/test.txt
+cp timer.py Day$Day/
+echo "from timer import time" > Day$Day/main.py
 
 Session=$(cat ./.cookie)
-echo $Session
 
 curl "https://adventofcode.com/2021/day/$Day/input" --cookie "session=$Session" > Day$Day/input.txt
